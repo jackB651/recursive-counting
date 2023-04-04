bfab7d665585c16f601d330a6e5b70debf5feeff
Jonathon Bialas <105708083+jackB651@users.noreply.github.com>
2023-04-04 18:22:36 -0500

    Add files via upload

---
+++ ruby/recursive_count.rb
@@ -0,0 +1,14 @@
+def recursive_count(num = 0)
+  # type your code in here
+  return if num >= 10
+  puts num
+  recursive_count(num + 1)
+end
+
+if __FILE__ == $PROGRAM_NAME
+  recursive_count
+end
+
+# OPTIONAL
+# Please add your pseudocode to this file
+# And a written explanation of your solution
